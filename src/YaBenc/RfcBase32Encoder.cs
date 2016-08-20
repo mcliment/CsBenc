2f27eb8233a731409afb53f5a3f6ebcd3c665999
Marc Climent <mcliment@gmail.com>
2016-08-21 01:30:26 +0200

    RFC4648 Base32 encoding
    No decoding yet!

---
+++ src/YaBenc/RfcBase32Encoder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YaBenc
+{
+    public class RfcBase32Encoder
+    {
+        private readonly static int bits = 5;
+        private readonly static int basE = 1 << bits;
+
+        private readonly static string _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+        private readonly static char _pad = '=';
+
+        public string Encode(string value)
+        {
+            var chunks = Chunker.GetChunks(value, bits);
+            var chars = chunks.Select(c => _alphabet[c]).ToArray();
+            var result = new string(chars);
+
+            while ((result.Length * bits % 8) != 0)
+            {
+                result += _pad;
+            }
+
+            return result;
+        }
+
+        public string Decode(string input)
+        {
+            var chars = GetChars(input).ToArray();
+
+            return new string(chars);
+        }
+
+        private IEnumerable<char> GetChars(string input)
+        {
+            for (var i = 0; i < input.Length; i += 2)
+            {
+                var l = _alphabet.IndexOf(input[i]);
+                var r = _alphabet.IndexOf(input[i + 1]);
+
+                yield return (char)((l << bits) + r);
+            }
+        }
+        
+    }
+}
---
+++ test/YaBenc.Tests/RfcBase32EncoderTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace YaBenc.Tests
+{
+    [TestFixture]
+    public class RfcBase32EncoderTests
+    {
+        private readonly RfcBase32Encoder encoder = new RfcBase32Encoder();
+
+        [Test]
+        public void Encodes_Test_Vectors()
+        {
+            encoder.Encode("").ShouldBe("");
+            encoder.Encode("f").ShouldBe("MY======");
+            encoder.Encode("fo").ShouldBe("MZXQ====");
+            encoder.Encode("foo").ShouldBe("MZXW6===");
+            encoder.Encode("foob").ShouldBe("MZXW6YQ=");
+            encoder.Encode("fooba").ShouldBe("MZXW6YTB");
+            encoder.Encode("foobar").ShouldBe("MZXW6YTBOI======");
+        }
+
+        //[Test]
+        //public void Decodes_Test_Vectors()
+        //{
+        //    encoder.Decode("").ShouldBe("");
+        //    encoder.Decode("MY======").ShouldBe("f");
+        //    encoder.Decode("MZXQ====").ShouldBe("fo");
+        //    encoder.Decode("MZXW6===").ShouldBe("foo");
+        //    encoder.Decode("MZXW6YQ=").ShouldBe("foob");
+        //    encoder.Decode("MZXW6YTB").ShouldBe("fooba");
+        //    encoder.Decode("MZXW6YTBOI======").ShouldBe("foobar");
+        //}
+    }
+}
