2179202b9f0d124a069b494cb4e6c3a73c46043d
Marc Climent <mcliment@gmail.com>
2016-08-29 23:51:57 +0200

    Added RFC Base32 Hex encoding

---
+++ src/YaBenc/RfcBase32HexEncoder.cs
@@ -0,0 +1,9 @@
+﻿namespace YaBenc
+{
+    public class RfcBase32HexEncoder : StringEncoder
+    {
+        public RfcBase32HexEncoder() : base("0123456789ABCDEFGHIJKLMNOPQRSTUV", '=')
+        {
+        }
+    }
+}
---
+++ test/YaBenc.Tests/RfcBase32HexEncoderTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace YaBenc.Tests
+{
+    [TestFixture]
+    public class RfcBase32HexEncoderTests
+    {
+        private readonly RfcBase32HexEncoder encoder = new RfcBase32HexEncoder();
+
+        [Test]
+        public void Encodes_Test_Vectors()
+        {
+            encoder.Encode("").ShouldBe("");
+            encoder.Encode("f").ShouldBe("CO======");
+            encoder.Encode("fo").ShouldBe("CPNG====");
+            encoder.Encode("foo").ShouldBe("CPNMU===");
+            encoder.Encode("foob").ShouldBe("CPNMUOG=");
+            encoder.Encode("fooba").ShouldBe("CPNMUOJ1");
+            encoder.Encode("foobar").ShouldBe("CPNMUOJ1E8======");
+        }
+
+        [Test]
+        public void Decodes_Test_Vectors()
+        {
+            encoder.Decode("").ShouldBe("");
+            encoder.Decode("CO======").ShouldBe("f");
+            encoder.Decode("CPNG====").ShouldBe("fo");
+            encoder.Decode("CPNMU===").ShouldBe("foo");
+            encoder.Decode("CPNMUOG=").ShouldBe("foob");
+            encoder.Decode("CPNMUOJ1").ShouldBe("fooba");
+            encoder.Decode("CPNMUOJ1E8======").ShouldBe("foobar");
+        }
+    }
+}
