57664e3b1cfe535e278741f7d7255c07a4d21863
Marc Climent <mcliment@gmail.com>
2016-08-29 23:51:34 +0200

    Refactor of string encoders

--- src/YaBenc/RfcBase16Encoder.cs
+++ src/YaBenc/RfcBase16Encoder.cs
@@ -1,45 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace YaBenc
+﻿namespace YaBenc
 {
-    public class RfcBase16Encoder
+    public class RfcBase16Encoder : StringEncoder
     {
-        private readonly static int bits = 4;
-        private readonly static int basE = 1 << bits;
-
-        private readonly static string _alphabet = "0123456789ABCDEF";
-
-        private readonly static StringProcessor _processor = new StringProcessor(bits);
-
-        public string Encode(string value)
+        public RfcBase16Encoder() : base("0123456789ABCDEF", '=')
         {
-            var chunks = _processor.Chunk(value);
-            var chars = chunks.Select(c => _alphabet[c]).ToArray();
-            var result = new string(chars);
-
-            return result;
         }
-
-        public string Decode(string input)
-        {
-            var chars = GetChars(input).ToArray();
-
-            return new string(chars);
-        }
-
-        private IEnumerable<char> GetChars(string input)
-        {
-            for (var i = 0; i < input.Length; i += 2)
-            {
-                var l = _alphabet.IndexOf(input[i]);
-                var r = _alphabet.IndexOf(input[i + 1]);
-
-                yield return (char)((l << bits) + r);
-            }
-        }
-        
     }
 }
--- src/YaBenc/RfcBase32Encoder.cs
+++ src/YaBenc/RfcBase32Encoder.cs
@@ -1,44 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace YaBenc
+﻿namespace YaBenc
 {
-    public class RfcBase32Encoder
+    public class RfcBase32Encoder : StringEncoder
     {
-        private readonly static int bits = 5;
-        private readonly static int basE = 1 << bits;
-
-        private readonly static string _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-        private readonly static char _pad = '=';
-
-        private readonly static StringProcessor _processor = new StringProcessor(bits);
-
-        public string Encode(string value)
+        public RfcBase32Encoder() : base("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567", '=')
         {
-            var chunks = _processor.Chunk(value);
-            var chars = chunks.Select(c => _alphabet[c]).ToArray();
-            var result = new string(_processor.Pad(chars, _pad));
-
-            return result;
         }
-
-        public string Decode(string input)
-        {
-            var chars = GetChars(input.TrimEnd(_pad)).ToArray();
-
-            return new string(chars);
-        }
-
-        private IEnumerable<char> GetChars(string input)
-        {
-            var values = input.TrimEnd(_pad).Select(i => _alphabet.IndexOf(i));
-
-            var result = _processor.Combine(values);
-
-            return result;
-        }
-        
     }
 }
