bd0fc280d06c9d2731569c15dc7455c87fb39d19
Marc Climent <mcliment@gmail.com>
2016-10-07 00:08:54 +0200

    Add Benchmark of base64 against .NET implementation (baseline)

---
+++ test/CsBenc.Benchmarks/Program.fs
@@ -0,0 +1,33 @@
+﻿module Program
+
+open System
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+open CsBenc.Strings
+
+type Base64Comparison () =
+    let N = 10000
+
+    let base64net = System.Convert.ToBase64String
+    let base64encoder = CsBenc.Strings.Encoder.RfcBase64()
+
+    let mutable data = Array.zeroCreate<byte> N
+
+    [<Setup>]
+    member self.SetupData () =
+        data <- Array.zeroCreate<byte> N
+        (new Random(42)).NextBytes(data)
+
+    [<Benchmark>]
+    member self.NetBase64 () = System.Convert.ToBase64String(data)
+
+    [<Benchmark>]
+    member self.OwnBase64 () = base64encoder.Encode(data)
+        
+
+let defaultSwitch () = BenchmarkSwitcher [| typeof<Base64Comparison> |]
+
+[<EntryPoint>]
+let main argv = 
+    let summary = defaultSwitch().Run argv
+    0 // return an integer exit code
