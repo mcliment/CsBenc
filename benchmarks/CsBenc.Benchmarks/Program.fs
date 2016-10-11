ec6ee5ef5491d2947f196f66c138dd29b31ca543
Marc Climent <mcliment@gmail.com>
2016-10-11 23:26:33 +0200

    Add decode benchmarks

--- benchmarks/CsBenc.Benchmarks/Program.fs
+++ benchmarks/CsBenc.Benchmarks/Program.fs
@@ -5,7 +5,7 @@ open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Running
 open CsBenc
 
-type Base64Comparison () =
+type Base64EncodingComparison () =
     let N = 10000
 
     let base64net = System.Convert.ToBase64String
@@ -19,12 +19,33 @@ type Base64Comparison () =
         (new Random(42)).NextBytes(data)
 
     [<Benchmark(Baseline = true)>]
-    member self.NetBase64 () = System.Convert.ToBase64String(data)
+    member self.NetBase64 () = base64net(data)
 
     [<Benchmark>]
     member self.OwnBase64 () = base64encoder.Encode(data)
 
-let defaultSwitch () = BenchmarkSwitcher [| typeof<Base64Comparison> |]
+type Base64DecodingComparison () =
+    let N = 10000
+
+    let base64net = System.Convert.FromBase64String
+    let base64decoder = CsBenc.Encoder.RfcBase64()
+
+    let mutable encoded = ""
+
+    [<Setup>]
+    member self.SetupData () =
+        let mutable data = Array.zeroCreate<byte> N
+        (new Random(42)).NextBytes(data)
+        encoded <- System.Convert.ToBase64String(data)
+
+    [<Benchmark(Baseline = true)>]
+    member self.NetBase64 () = base64net(encoded)
+
+    [<Benchmark>]
+    member self.OwnBase64 () = base64decoder.Decode(encoded)
+
+
+let defaultSwitch () = BenchmarkSwitcher [| typeof<Base64EncodingComparison>; typeof<Base64DecodingComparison> |]
 
 [<EntryPoint>]
 let main argv = 
